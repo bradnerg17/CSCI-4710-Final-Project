210a62e9441fe8e5470dcd10d0138356377a2632
Steven Casper <sebastiancasper3@gmail.com>
2020-04-28 17:46:12 -0400

    Fix sidebar

---
+++ PatternGen - Copy/templates/static/sidebar.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+        $(".newbg").click(function() {
+            newGrad('all');
+        });
+        $(".newcolor").click(function() {
+            newGrad('color');
+        });
+        $(".newpattern").click(function() {
+            newGrad('pattern');
+        });
+    });
